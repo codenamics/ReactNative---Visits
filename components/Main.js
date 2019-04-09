61e30305b1b7eb9e3491efe7dc36afe104a4ebe6
codenamics <info@codenamics.pl>
2019-04-09 23:18:23 +0200

    visits cdn

--- components/Main.js
+++ components/Main.js
@@ -1,7 +1,40 @@
 import React, { Component } from "react";
-import { Text, StyleSheet, View, ScrollView, Dimensions } from "react-native";
+import {
+  Text,
+  StyleSheet,
+  FlatList,
+  View,
+  ScrollView,
+  Dimensions,
+  ImageBackground
+} from "react-native";
 
 const { width, height } = Dimensions.get("screen");
+
+const mocks = [
+  {
+    id: 1,
+    title: "Visit 1",
+    date: "28-02-2019",
+    url:
+      "https://images.pexels.com/photos/1001752/pexels-photo-1001752.jpeg?cs=srgb&dl=checking-checklist-daily-report-1001752.jpg&fm=jpg"
+  },
+  {
+    id: 2,
+
+    title: "Visit 2",
+    date: "01-03-2019",
+    url:
+      "https://images.pexels.com/photos/1001752/pexels-photo-1001752.jpeg?cs=srgb&dl=checking-checklist-daily-report-1001752.jpg&fm=jpg"
+  },
+  {
+    id: 3,
+    title: "Visit 3",
+    date: "03-03-2019",
+    url:
+      "https://images.pexels.com/photos/1001752/pexels-photo-1001752.jpeg?cs=srgb&dl=checking-checklist-daily-report-1001752.jpg&fm=jpg"
+  }
+];
 
 const styles = StyleSheet.create({
   flex: {
@@ -25,18 +58,41 @@ const styles = StyleSheet.create({
   visit: {
     width: width - 36 * 2,
     borderRadius: 12,
-    padding: 36,
+    paddingHorizontal: 36,
     marginHorizontal: 36,
-    backgroundColor: "pink"
+    backgroundColor: "#55a8fd",
+    position: "relative"
+  },
+  visitDate: {
+    paddingHorizontal: 36,
+    position: "absolute",
+    bottom: -20,
+    right: 60,
+    left: 60,
+    padding: 12,
+    borderRadius: 12,
+    backgroundColor: "white"
+  },
+
+  shadow2: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 4,
+      height: 6
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 10.32,
+
+    elevation: 16
   }
 });
-export default class Main extends Component {
+class Main extends Component {
   static navigationOptions = {
     header: (
       <View style={[styles.flex, styles.row, styles.header]}>
         <View>
           <Text>Search for</Text>
-          <Text>Destination</Text>
+          <Text>Your visits</Text>
         </View>
         <View>
           <Text>Avatar</Text>
@@ -48,32 +104,45 @@ export default class Main extends Component {
   renderVisits = () => {
     return (
       <View style={[styles.flex, styles.column]}>
-        <ScrollView
+        <FlatList
           horizontal
           pagingEnabled
           scrollEnabled
           showsHorizontalScrollIndicator={false}
           scrollEventThrottle={16}
+          style={[{ overflow: "visible" }, styles.shadow2]}
           snapToAlignment="center"
-        >
-          <View style={[styles.flex, styles.visit]}>
-            <Text>Visits 1</Text>
-          </View>
-          <View style={[styles.flex, styles.visit]}>
-            <Text>Visits 2</Text>
-          </View>
-          <View style={[styles.flex, styles.visit]}>
-            <Text>Visits 3</Text>
-          </View>
-        </ScrollView>
+          data={this.props.visits}
+          keyExtractor={(item, index) => `${item.id}`}
+          renderItem={({ item }) => this.renderVisit(item)}
+        />
       </View>
     );
   };
 
+  renderVisit = item => {
+    return (
+      <View style={{ position: "relative" }}>
+        <View
+          style={[
+            styles.flex,
+            styles.row,
+            styles.visit,
+            { alignItems: "center", justifyContent: "center" }
+          ]}
+        >
+          <Text style={{ fontSize: 40 }}>{item.title}</Text>
+        </View>
+        <View style={[styles.visitDate, { alignItems: "center" }]}>
+          <Text style={{ fontSize: 25 }}>{item.date}</Text>
+        </View>
+      </View>
+    );
+  };
   renderDrugs = () => {
     return (
       <View style={[styles.flex, styles.column]}>
-        <Text>Drugs</Text>
+        <Text />
       </View>
     );
   };
@@ -87,3 +156,9 @@ export default class Main extends Component {
     );
   }
 }
+
+Main.defaultProps = {
+  visits: mocks
+};
+
+export default Main;
